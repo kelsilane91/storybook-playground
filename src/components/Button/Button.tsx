8e812cd8ed457dbf484f4200d3703fdc852e9f5c
kelsilane91 <kelsilane91@gmail.com>
2023-05-24 20:27:33 +0000

    add new prop

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,25 @@
 import { Button } from "@mui/material";
 
-export const CustomButton = ({ label }: { label: string }) => {
+export const CustomButton = ({
+  label,
+  color,
+}: {
+  label: string;
+  color:
+    | "inherit"
+    | "primary"
+    | "secondary"
+    | "success"
+    | "error"
+    | "info"
+    | "warning";
+}) => {
   return (
     <Button
       onClick={() => {
         alert("clicked");
       }}
+      color={color}
     >
       {label}
     </Button>
