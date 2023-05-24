6aec77cb5c8758b1745b1bad40544220d4202021
kelsilane91 <kelsilane91@gmail.com>
2023-05-24 20:34:36 +0000

    updates

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,11 @@
 import type { Preview } from "@storybook/react";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/material-icons';
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
